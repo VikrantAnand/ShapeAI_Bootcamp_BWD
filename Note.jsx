4d61ab3e58440d7cea5c3a857bcaf751dcee7764
VikrantAnand <85404884+VikrantAnand@users.noreply.github.com>
2021-06-06 13:26:48 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -4,7 +4,7 @@ function Note() {
   return (
     <div className="note">
       <h1>Bootcamp on Reactjs And Javascript</h1>
-      <p>I have attended this bootcamp and learned basics of Web development</p>
+      <p>Myself Vikrant Anand I have attended this bootcamp and learned basics of Web development</p>
     </div>
   );
 }
